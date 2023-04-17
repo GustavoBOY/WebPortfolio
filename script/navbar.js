aff0917523182b864e3332e6b5a9ff21ac394f66
GustavoBOY <gustavoboy1702@gmail.com>
2023-04-17 00:18:17 -0300

    Adição experiencias e ajuste nav

--- script/navbar.js
+++ script/navbar.js
@@ -1,3 +1,6 @@
 function toogleSidebar(){
   document.getElementById("sidebar").classList.toggle("active");
 }
+function toogleSidebarClosed(){
+  document.getElementById("sidebar").classList.toggle("active");
+}
